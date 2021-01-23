086a5c64e9b3cb0d234834e1948c9f2e38fdee25
Leo Yang <leo19941227@gmail.com>
2021-01-23 17:01:35 +0800

    Debug

--- user_data_script.sh
+++ user_data_script.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
 EFS_ID="fs-33f71937"
+file_system_id_1=$EFS_ID
+efs_mount_point_1=/mnt/efs/fs1/
 
-# update packages
-sudo yum -y update
-sudo apt-get -y update
+if [ "$(cat /etc/os-release | head -n 1)" == "NAME=\"Amazon Linux\"" ]; then
+    sudo yum -y update
+    sudo yum -y upgrade
 
-sudo yum -y upgrade
-sudo apt-get -y upgrade
+    yum install -y amazon-efs-utils
+    yum install -y nfs-utils
 
-# install packages for efs
-yum install -y amazon-efs-utils
-apt-get -y install amazon-efs-utils
+    CONDA_ROOT=$efs_mount_point_1"/miniconda3_amazon_linux2/"
+elif [ "$(cat /etc/os-release | head -n 1)" == "NAME=\"Ubuntu\"" ]; then
+    sudo apt-get -y update
+    sudo apt-get -y upgrade
 
-yum install -y nfs-utils
-apt-get -y install nfs-common
+    git clone https://github.com/aws/efs-utils
+    cd efs-utils/
+    sudo apt install make
+    sudo apt install binutils
+    ./build-deb.sh
+    sudo apt install -y ./build/$(ls ./build | grep amazon-efs-utils)
+    cd ../
+
+    CONDA_ROOT=$efs_mount_point_1"/miniconda3_ubuntu18/"
+fi
 
 # mount efs
-file_system_id_1=$EFS_ID
-efs_mount_point_1=/mnt/efs/fs1
 mkdir -p "${efs_mount_point_1}"
 test -f "/sbin/mount.efs" && printf "\n${file_system_id_1}:/ ${efs_mount_point_1} efs tls,_netdev\n" >>/etc/fstab || printf "\n${file_system_id_1}.efs.us-west-2.amazonaws.com:/ ${efs_mount_point_1} nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport,_netdev 0 0\n" >>/etc/fstab
 test -f "/sbin/mount.efs" && printf "\n[client-info]\nsource=liw\n" >>/etc/amazon/efs/efs-utils.conf
@@ -38,18 +47,12 @@ done
 WAIT_BEFORE_EXECUTION=10
 sleep $WAIT_BEFORE_EXECUTION
 
-if [ $(cat /etc/os-release | head -n 1) == "NAME=\"Ubuntu\"" ]; then
-    CONDA_ROOT=$efs_mount_point_1"miniconda3_ubuntu18/"
-
-elif [ $(cat /etc/os-release | head -n 1) == "NAME=\"Amazon Linux\"" ]; then
-    CONDA_ROOT=$efs_mount_point_1"miniconda3_amazon_linux2/"
-
 SESSION=work
 sudo -H -u ec2-user tmux new -ds $SESSION
-sudo -H -u ec2-user tmux send -t $SESSION "cd \$efs_mount_point_1; \
-source \$CONDA_ROOT\"/bin/activate\" benchmark; \
+sudo -H -u ec2-user tmux send -t $SESSION "cd ${efs_mount_point_1}; \
+source ${CONDA_ROOT}\"/bin/activate\" benchmark; \
 LOGNAME=\"COMMAND_PLACEHOLDER\"; \
-LOGDIR=\$efs_mount_point_1\"/logs/\"\${LOGNAME// /_}; \
+LOGDIR=${efs_mount_point_1}\"/logs/\"\${LOGNAME// /_}; \
 mkdir -p \$LOGDIR; \
 cd WORKDIR_PLACEHOLDER; \
 AWS_REGION=\$(curl http://169.254.169.254/latest/dynamic/instance-identity/document | grep region | awk -F\\\" '{print \$4}'); \
