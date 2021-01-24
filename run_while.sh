73a1a78fd853c8560510cf659a023b0aba4c784c
Leo Yang <leo19941227@gmail.com>
2021-01-24 12:25:38 +0800

    Revert "Terminate if run_while.sh"
    This reverts commit 34fe5fa847a8237ad06806daa51f6289d021512c.

--- run_while.sh
+++ run_while.sh
@@ -3,9 +3,6 @@
 for i in $(seq 1 100);
 do
     eval $*
-    if [ ! -z $TERMINATE_COMMAND ]; then
-        eval $TERMINATE_COMMAND
-    fi
     echo Issue the same command again in 10 secs...
     sleep 10
 done
